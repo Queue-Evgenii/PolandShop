98ed13067d424496e1de7c553affc1524001559c
Queue-Evgenii <genadolgii228@gmail.com>
2023-03-11 20:52:17 +0200

    chain add

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "/shop"
+  publicPath: "/shop",
+  chainWebpack: config => {
+    config.plugin('fork-ts-checker').tap(args => {
+      args[0].memoryLimit = 4096;
+      return args;
+    });
+  },
 })
