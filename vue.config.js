58f7a74ab2675c4a29a49c00cacc43f3174d7f7f
Queue-Evgenii <genadolgii228@gmail.com>
2023-03-13 15:30:11 +0200

    newnew

--- vue.config.js
+++ vue.config.js
@@ -1,32 +1,6 @@
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: "/shop",
-  configureWebpack: {
-    plugins: [
-      new CopyWebpackPlugin({
-        patterns: [
-          {
-            from: 'public/.htaccess',
-            to: 'dist/.htaccess'
-          }
-        ]
-      })
-    ],
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules\/(?!(@?(your-company|your-package)\/))/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              ignore: [/node_modules\/(@?(your-company|your-package)\/).*\.js/]
-            }
-          }
-        }
-      ]
-    }
-  }
 })
