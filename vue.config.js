a91a457f035c97e2e20bdd137e84d7a063d8ac1f
Queue-Evgenii <genadolgii228@gmail.com>
2023-03-13 14:54:43 +0200

    new htaccess

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,18 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: "/shop",
+  configureWebpack: {
+    plugins: [
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: 'public/.htaccess',
+            to: 'dist/.htaccess'
+          }
+        ]
+      })
+    ]
+  }
 })
